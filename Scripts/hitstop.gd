a492d63ceb5949553bb69cfbfba760595ef7e3f6
Jairo <jairospm2002@gmail.com>
2025-12-04 18:26:50 -0500

    Hitstop and hitflash work on player now

--- Scripts/hitstop.gd
+++ Scripts/hitstop.gd
@@ -16,7 +16,6 @@ func hit_stop(attackType : AttackType):
 			duration = 0.20
 		AttackType.SPECIAL:
 			duration = 0.30
-	
 	Engine.time_scale = 0
 	await get_tree().create_timer(duration, true, false, true).timeout
 	Engine.time_scale = 1
